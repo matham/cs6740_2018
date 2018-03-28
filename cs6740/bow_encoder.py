1de8ef8f531ddf46f416811a51ee77c177140aee
James Stoyell <jms852@cornell.edu>
2018-03-28 17:57:09 -0400

    use only a single hidden layer for bow encoder

--- cs6740/bow_encoder.py
+++ cs6740/bow_encoder.py
@@ -6,16 +6,13 @@ class BOWEncoder(nn.Module):
         super(BOWEncoder, self).__init__()
         self.layer_size = layer_size
 
-        self.layer_one = nn.linear(input_size, layer_size)
-        self.layer_two = nn.linear(layer_size, output_size)
-        self.activation = nn.ReLU()
+        self.hidden_layer = nn.linear(input_size, output_size)
 
     def forward(self, in_data):
         # Max pool input word vectors
         # Assuming input of shape (batch_size, sentence_length, embedding_size)
         pool = torch.max(in_data, 1)
-        l1 = self.activation(self.layer_one(pool))
-        l2 = self.layer_two(l1)
+        out_layer = self.hidden_layer(pool)
         # normalize the output layer
-        norm = torch.norm(l2, 2, 1, keep_dim=True)
-        return l2/norm
+        norm = torch.norm(out_layer, 2, 1, keep_dim=True)
+        return out_layer/norm
