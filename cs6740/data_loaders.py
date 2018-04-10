d80351ed89abcd52666ed45378e643fdfea10284
Matt Einhorn <moiein2000@gmail.com>
2018-04-09 20:25:32 -0400

    Use +/- examples for each pair.

--- cs6740/data_loaders.py
+++ cs6740/data_loaders.py
@@ -4,6 +4,23 @@ import random
 
 class CocoDataset(dset.CocoCaptions):
 
-    def __getitem__(self, item):
-        img, captions = item
-        return img, captions[random.randint(0, len(captions) - 1)]
+    def __init__(self, *largs, **kwargs):
+        super(CocoDataset, self).__init__(*largs, **kwargs)
+        n = len(self.ids)
+        # for every example, create a negative example and store them i >= len(ids)
+        mapping = self.data_mapping = list(range(2 * n))
+
+        for i in range(n):
+            val = i
+            while val == i:
+                val = random.randint(0, n - 1)
+            mapping[n + i] = val
+
+    def __getitem__(self, index):
+        img, captions = super(CocoDataset, self).__getitem__(index)
+        caption = captions[random.randint(0, len(captions) - 1)]
+        label = 1 if index < len(self.data_mapping) else 0
+        return img, caption, label
+
+    def __len__(self):
+        return len(self.data_mapping)
